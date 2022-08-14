f8c27501bdc31b54eb7f7900666216b48beca82c
shogo61 <shogo.611.lol@gmail.com>
2022-08-14 17:39:29 +0900

    配列格納

---
+++ test/KeyEvent.js
@@ -0,0 +1,17 @@
+//idの付与・削除
+function onclickTd(td){
+  let element = document.getElementById("target");
+  element.removeAttribute("id");
+  td.id = 'target'
+}
+
+//キー入力
+document.addEventListener('keypress', keypress_event);
+function keypress_event(e) {
+  if(e.key==1 || e.key==2 || e.key==3 || e.key==4 || e.key==5 || e.key==6 || e.key==7 || e.key==8 || e.key==9){
+    //#targetに数字を入力する
+    document.getElementById('target').innerHTML = e.key;
+  }
+  console.log(e);
+	return false; 
+}
